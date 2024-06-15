cff19f00b462bc7f3a42dfc1e92e2db2037bd926
afivve <afivve@gmail.com>
2024-06-15 19:47:13 +0700

    # Update Schema Validation

--- src/routes/discussion.route.js
+++ src/routes/discussion.route.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const controllerDiscussion = require('../controllers/discussion/discussion.controller')
 const controllerCommentar = require('../controllers/discussion/discussion.commentar.controller')
-/* const validate = require('../middlewares/validation') */
-/* const schema = require('../validations/auth.schema') */
+const validate = require('../middlewares/validation')
+const schema = require('../validations/discussion.schema')
 const { verifyToken } = require('../middlewares/verify.token')
 const multer = require('multer')()
 
 const router = express.Router()
 
-router.post('/discussions', verifyToken, multer.single("photoDiscussion"), controllerDiscussion.create)
-router.put('/discussions/:id', verifyToken, multer.single("photoDiscussion"), controllerDiscussion.update)
+router.post('/discussions', multer.single("photoDiscussion"), verifyToken, validate(schema.discussion), controllerDiscussion.create)
+router.put('/discussions/:id', multer.single("photoDiscussion"), verifyToken, validate(schema.discussion), controllerDiscussion.update)
 router.get('/discussions/:id', controllerDiscussion.readById)
 router.get('/discussions/', controllerDiscussion.readAll)
 router.delete('/discussions/:id', verifyToken, controllerDiscussion.delete)
 
 
 /* Commentar Route */
-router.post('/discussions/:discussionId/commentar', verifyToken, multer.single("photoCommentar"), controllerCommentar.create)
-router.put('/discussions/:discussionId/commentar/:id', verifyToken, multer.single("photoCommentar"), controllerCommentar.update)
+router.post('/discussions/:discussionId/commentar', verifyToken, multer.single("photoCommentar"), validate(schema.commentar), controllerCommentar.create)
+router.put('/discussions/:discussionId/commentar/:id', verifyToken, multer.single("photoCommentar"), validate(schema.commentar), controllerCommentar.update)
 router.delete('/discussions/:discussionId/commentar/:id', verifyToken, controllerCommentar.delete)
 
 module.exports = router
---
+++ src/validations/discussion.schema.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator")
+
+module.exports = {
+
+    discussion: [
+        body("title")
+            .notEmpty().withMessage("Nama tidak boleh kosong")
+            .isLength({ min: 4 }).withMessage("Terlalu pendek")
+            .isString().withMessage("Nama harus berupa string"),
+        body("question")
+            .notEmpty().withMessage("Nama tidak boleh kosong")
+            .isLength({ min: 4 }).withMessage("Terlalu pendek")
+            .isString().withMessage("Nama harus berupa string"),
+    ],
+    commentar: [
+        body("commentar")
+            .notEmpty().withMessage("Nama tidak boleh kosong")
+            .isLength({ min: 4 }).withMessage("Terlalu pendek")
+            .isString().withMessage("Nama harus berupa string"),
+    ],
+
+}
